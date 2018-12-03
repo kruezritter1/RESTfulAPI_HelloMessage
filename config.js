7351af1e82971d7121ef5e9cc36bd6dce712e8c0
kruezritter1 <45561443+kruezritter1@users.noreply.github.com>
2018-12-03 21:12:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
 
+var environments={};
+environments.staging={
+'httpport':3000,
+  'httpsport':3001,
+    'envName':'Staging'
+};
+environments.production={
+'httpport':5000,
+  'httpsport':5001,
+    'envName':'Production'
+};
+var selectedEnv=typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+var exportedEnv=typeof(environments[selectedEnv])== 'object'? environments[selectedEnv] : environments.staging;
+module.exports=exportedEnv;
